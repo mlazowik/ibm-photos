7226ae8e27507e6d932f8cb5341323d9a0d6230d
Michał Łazowik <mlazowik@gmail.com>
2019-06-11 03:52:45 +0200

    Site shell

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "carbon-components/css/carbon-components.min.css";
+import {
+  Content,
+  Header,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+  HeaderName
+} from "carbon-components-react";
+
+import Search20 from "@carbon/icons-react/es/search/20";
+import Notification20 from "@carbon/icons-react/es/notification/20";
+import AppSwitcher20 from "@carbon/icons-react/es/app-switcher/20";
+
+const noop = () => {};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header aria-label="IBM Platform Name">
+        <HeaderName href="#">Photos</HeaderName>
+        <HeaderGlobalBar>
+          <HeaderGlobalAction aria-label="Search" onClick={noop}>
+            <Search20 />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction
+            aria-label="Notifications"
+            isActive
+            onClick={noop}
+          >
+            <Notification20 />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="App Switcher" onClick={noop}>
+            <AppSwitcher20 />
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
+      </Header>
+      <Content>
+        <p>Test</p>
+      </Content>
+    </>
   );
 }
 
