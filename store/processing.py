5bb21952926f9c40039f882a82555e706f901462
Michał Łazowik <mlazowik@gmail.com>
2019-06-13 02:51:22 +0200

    Async ml requests

--- store/processing.py
+++ store/processing.py
@@ -1,36 +1,51 @@
-import requests
+import grequests
 from django.db import transaction
 
 from store.models import Photo, Object
 
 API_URL = "http://max-object-detector.max.us-south.containers.appdomain.cloud/model/predict"
+BATCH_SIZE = 10
+
+
+def chunks(list_to_split, chunk_size):
+    for i in range(0, len(list_to_split), chunk_size):
+        yield list_to_split[i:i + chunk_size]
+
+
+def get_unsent_api_request(photo):
+    files = {'image': photo.image.file}
+    values = {'threshold': 0.5}
+
+    return grequests.post(API_URL, files=files, data=values)
+
+
+def save_result(result, photo):
+    data = result.json()
+
+    if data['status'] == "ok":
+        for p in data['predictions']:
+            Object.objects.create(
+                photo=photo,
+                label=p['label'],
+                probability=p['probability'],
+                y_min=p['detection_box'][0],
+                x_min=p['detection_box'][1],
+                y_max=p['detection_box'][2],
+                x_max=p['detection_box'][3]
+            )
+
+        photo.processed = True
+        photo.save()
 
 
 def process_photos():
     photo_ids = list(Photo.objects.filter(processed=False).values_list('pk', flat=True))
 
-    session = requests.Session()
-
-    for photo_id in photo_ids:
+    for photo_ids_chunk in chunks(photo_ids, BATCH_SIZE):
         with transaction.atomic():
-            photo = Photo.objects.get(pk=photo_id)
-            files = {'image': photo.image.file}
-            values = {'threshold': 0.5}
+            photos = Photo.objects.filter(pk__in=photo_ids_chunk)
+            unsent_api_requests = map(get_unsent_api_request, photos)
+            results = grequests.map(unsent_api_requests)
 
-            r = session.post(API_URL, files=files, data=values)
-            data = r.json()
-
-            if data['status'] == "ok":
-                for p in data['predictions']:
-                    Object.objects.create(
-                        photo=photo,
-                        label=p['label'],
-                        probability=p['probability'],
-                        y_min=p['detection_box'][0],
-                        x_min=p['detection_box'][1],
-                        y_max=p['detection_box'][2],
-                        x_max=p['detection_box'][3]
-                    )
-
-                photo.processed = True
-                photo.save()
+            for (r, p) in zip(results, photos):
+                save_result(r, p)
