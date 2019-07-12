a3e2c19a042cac440868d34cd2640a282ea6f50b
Michał Łazowik <mlazowik@gmail.com>
2019-07-12 22:55:43 +0200

    Remove api token url endpoint
    Tokens can be set in django admin

--- back/store/urls.py
+++ back/store/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from rest_framework.authtoken import views
 
 from store.views import PhotoListView, PhotoViewSet
 
 store_urls = [
-    path('api-token-auth/', views.obtain_auth_token),
     path('photo/', PhotoViewSet.as_view({'post': 'create'})),
     path('photos/', PhotoListView.as_view())
 ]
